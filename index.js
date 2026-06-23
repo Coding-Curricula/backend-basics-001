b43e49b3e5c74a106103afbe7483d0957a0f2668
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2026-06-23 14:16:42 -0500

    .gitignore nodemon prettier devDep stuff

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = 8080;
 
 app.get("/", (request, response) => {
-  response.send("with REQ / RES: a string that says something");
+  response.send("hi again nodemon - as expected");
 });
 
-app.listen(PORT, 
-    () => { console.log(`Server is running on port ${PORT}`); 
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
