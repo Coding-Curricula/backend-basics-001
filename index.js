3c44903d989a68b16a8f5f39e5d95067e46d3584
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2026-06-23 14:38:22 -0500

    more api endpoints

--- index.js
+++ index.js
@@ -3,8 +3,26 @@ const app = express();
 
 const PORT = 8080;
 
-app.get("/", (request, response) => {
-  response.send("hi again nodemon - as expected");
+// GET - api/v1/health - returns a simple message to indicate that the server is running
+app.get("/api/v1/health", (request, response) => {
+  response.send("ALIVE AND WELL ✅ ");
+});
+
+// GET - api/v1/hello?name=string - returns a simple message to indicate that the server is running
+app.get("/api/v1/hello", (request, response) => {
+  const name = request.query.name || "no name provided";
+  response.send(`Hello ${name} 👋`);
+});
+
+// GET - api/v1/weather?city=string&name=string - returns a simple message to indicate that the server is running
+app.get("/api/v1/weather", (request, response) => {
+  const city = request.query.city || "no city provided";
+  const name = request.query.name || "no name provided";
+  response.send({
+    city: city,
+    temperature: "25°C",
+    condition: `Its hot and sunny ${name} in ${city}`,
+  });
 });
 
 app.listen(PORT, () => {
